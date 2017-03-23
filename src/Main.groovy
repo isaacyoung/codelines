cd4c2e1a8bf4f3402d2428efc98c29ca0ca097a8
yzh <48515824@qq.com>
2017-03-23 19:07:53 +0800

    stat java

--- src/Main.groovy
+++ src/Main.groovy
@@ -8,6 +8,14 @@
 class Main {
 
     static void main(args) {
-
+        def path = 'E:\\code\\dfws-manage\\' // 目录
+        File f = new File(path)
+        Result result = StatJava.stat(f)
+        System.out.println("path:" + f.getPath())
+        System.out.println("class:" + result.classcount)
+        System.out.println("blankLines:" + result.blankLines)
+        System.out.println("commentLines:" + result.commentLines)
+        System.out.println("codeLines:" + result.codeLines)
     }
+
 }
---
+++ src/StatJava.groovy
@@ -0,0 +1,72 @@
+/**
+ * 项目名称：代码统计
+ * 类描述：统计 java groovy
+ * 创建人：yzh
+ * 创建时间：2017/3/23
+ * 备注：
+ */
+class StatJava {
+
+    /**
+     * 统计入口
+     * @param f
+     * @return
+     */
+    static Result stat(File f) {
+        def result = new Result()
+        loop(f,result)
+        result
+    }
+
+    /**
+     * 循环遍历
+     * @param f
+     * @param result
+     */
+    static void loop(File f,Result result) {
+        File[] childs = f.listFiles()
+        for (int i = 0; i < childs.length ;i++) {
+            File file = childs[i]
+            if (!file.isDirectory()) {
+                if (file.getName().toLowerCase().endsWith(".java")) {
+                    result.classcount++
+                    BufferedReader br = null
+                    boolean comment = false
+                    br = new BufferedReader(new FileReader(file))
+                    String line = ""
+
+                    while ((line = br.readLine()) != null) {
+                        line = line.trim()
+
+                        if (line.startsWith("/*") && line.endsWith("*/")) {
+                            result.commentLines++
+                        } else if (line.startsWith("/*") && !line.endsWith("*/")) {
+                            result.commentLines++
+                            comment = true
+                        } else if (comment) {
+                            result.commentLines++
+                            if (line.endsWith("*/")) {
+                                comment = false
+                            }
+                        } else if (line.startsWith("//")) {
+                            result.commentLines++
+                        } else if (line == "") {
+                            result.blankLines++
+                        } else {
+                            result.codeLines++
+                        }
+                    }
+                    if (br != null) {
+                        br.close()
+                        br = null
+                    }
+                }
+            } else {
+                loop(childs[i],result)
+            }
+        }
+
+        result
+    }
+
+}
